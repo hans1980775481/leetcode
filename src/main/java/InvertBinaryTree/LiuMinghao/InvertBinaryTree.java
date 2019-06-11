d16bff1babcc862c4d1859e9366a3628e25a92bc
L先生 <1159679566@qq.com>
2019-06-11 15:52:47 +0800

    reversalTree

---
+++ src/main/java/InvertBinaryTree/LiuMinghao/InvertBinaryTree.java
@@ -0,0 +1,35 @@
+package InvertBinaryTree.LiuMinghao;
+
+public class InvertBinaryTree {
+
+    public TreeNode InvertBinaryTree(TreeNode treeNode) {
+        if (treeNode == null)
+            return null;
+        reversalTree(treeNode);
+        return treeNode;
+    }
+
+    public void reversalTree(TreeNode treeNode) {
+        if (treeNode != null) {
+            reversalTree(treeNode.left);
+            reversalTree(treeNode.right);
+            TreeNode temp;
+            temp = treeNode.left;
+            treeNode.left = treeNode.right;
+            treeNode.right = temp;
+        }
+    }
+
+
+}
+
+// Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
