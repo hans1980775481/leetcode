724f394c5b4e7f0f1d5a94490a96769a08a24a37
L先生 <1159679566@qq.com>
2019-06-11 20:47:28 +0800

    递归求二叉树最大路径

--- src/main/java/MaximumDepthOfBinaryTree/LiuMinghao/MaximumDepthofBinaryTree.java
+++ src/main/java/MaximumDepthOfBinaryTree/LiuMinghao/MaximumDepthofBinaryTree.java
@@ -7,7 +7,7 @@ public class MaximumDepthofBinaryTree {
         if(treeNode == null) {
             return 0;
         }
-        //递归求出最深的层数 每层递归+1 遇到子节点返回0
+        //递归求出最深的层数 每层递归+1 遇到子节 点返回0
         return 1+MaxDeep(treeNode.left)>MaxDeep(treeNode.right)?MaxDeep(treeNode.left):MaxDeep(treeNode.right);
     }
 }
