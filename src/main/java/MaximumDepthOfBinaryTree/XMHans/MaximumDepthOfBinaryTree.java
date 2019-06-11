e2130c5a0179895361a6b68ff9a87331a538bd24
XMHans <1980775481@qq.com>
2019-06-11 15:11:42 +0800

    完成Maximum Depth of Binary Tree【二叉树的高度】 第二种方法（也是用递归）

--- src/main/java/MaximumDepthOfBinaryTree/XMHans/MaximumDepthOfBinaryTree.java
+++ src/main/java/MaximumDepthOfBinaryTree/XMHans/MaximumDepthOfBinaryTree.java
@@ -14,6 +14,20 @@ public class MaximumDepthOfBinaryTree {
         // RMax = getMaxDepth(root.right,len+1); // 不能用里那是变量将这个结果存起来，因为在底柜的过程中这两个临时变量一直被赋值，一直在变会有BUG的
         return Math.max(getMaxDepth(root.left,len+1),getMaxDepth(root.right,len+1));
     }
+
+    // 第二种方法递归
+    public int maxDepthTwoMethod(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left != null && root.right == null) {
+            return 1+maxDepth(root.left);
+        } else if (root.left == null && root.right != null) {
+            return 1+maxDepth(root.right);
+        } else if (root.left != null && root.right != null){
+            return 1+Math.max(maxDepth(root.right),maxDepth(root.left));
+        } else {
+            return 1;
+        }
+    }
 }
 
 // Definition for a binary tree node.
