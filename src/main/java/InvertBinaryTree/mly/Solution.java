3fa1e693e9a95d117a7862094d86d3df3dfe76cd
maliyuan <liyuan.ma@hpe-airchina.com>
2019-06-12 18:48:54 +0800

    mly-Invert Binary Tree

---
+++ src/main/java/InvertBinaryTree/mly/Solution.java
@@ -0,0 +1,23 @@
+package InvertBinaryTree.mly;
+
+
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root==null){
+            return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
