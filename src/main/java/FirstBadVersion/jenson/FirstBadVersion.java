aee921e323722e577dff3ffea0cdeef775ae2706
ZhouSong <song_zs@mail.everythingknow.cn>
2019-06-17 15:40:26 +0800

    add FirstBadVersion.java

---
+++ src/main/java/FirstBadVersion/jenson/FirstBadVersion.java
@@ -0,0 +1,25 @@
+package FirstBadVersion.jenson;
+
+/**
+ * @author: zhousong
+ * @data: 2019/6/17
+ */
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean isBadVersion(int version) {
+        return true;
+    }
+}
