f95be1be9a3f25cba912a4c58e025d54af8226a5
yanxianni <yanerxianni@sina.com>
2019-06-19 15:53:18 +0800

    modify BinaryTree

--- src/main/java/InvertBinaryTree/yanxianni/InvertBinaryTree.java
+++ src/main/java/InvertBinaryTree/yanxianni/InvertBinaryTree.java
@@ -1,22 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public TreeNode invertTree(TreeNode root) {
-        if(root == null){
-            return root;
-        }
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        
-        root.right = invertTree(left);
-        root.left = invertTree(right);
-        return root;
-    }
+public class InvertBinaryTree {
+	public TreeNode invertTree(TreeNode root) {
+		//结束条件
+		if (root == null) {
+			return root;
+		}
+		TreeNode left = root.left;
+		TreeNode right = root.right;
+
+		root.right = invertTree(left);
+		root.left = invertTree(right);
+		return root;
+	}
+
+	// 定义二叉树
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
 }
