d6500bce87a4106628b7946e3f182586c294f9fd
wxnewtouch <761266594@qq.com>
2019-06-24 15:07:43 +0800

    RemoveElements

---
+++ src/main/java/RemoveElement/wally/RemoveElement.java
@@ -0,0 +1,22 @@
+package RemoveElement.wally;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 这道题的空间空间复杂度为O1
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums,int val){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != val){
+                list.add(nums[i]);
+            }
+        }
+        for (int i = 0; i < list.size(); i++){
+            nums[i] = list.get(i);
+        }
+        return list.size();
+    }
+}
