bec47950b71ee1084db1a1946aa6368f072197f1
HZ_Fang <hzfone@163.com>
2019-06-24 23:44:53 +0800

    codeLove --FirstBadVersion commit

--- src/main/java/MoveZeroes/CodeLove/MoveZeroes.java
+++ src/main/java/MoveZeroes/CodeLove/MoveZeroes.java
@@ -1,4 +1,13 @@
 package MoveZeroes.CodeLove;
 
 public class MoveZeroes {
+    public void moveZeros(int[] nums){
+        for (int i = 0, cur = 0; i< nums.length; i++) {
+            if (nums[i] !=0) {
+                int swap = nums[i];
+                nums[i] = nums[cur];
+                nums[cur++] = swap;
+            }
+        }
+    }
 }
