1dc5204a89e1d3fcaedf649207a9647fd8b3b51e
Azusa mutan <121443662@qq.com>
2019-07-05 14:34:18 +0800

    new

--- src/main/java/GuessNumberHigherorLowerII/AzusaMutan/Solution.java
+++ src/main/java/GuessNumberHigherorLowerII/AzusaMutan/Solution.java
@@ -1,4 +1,19 @@
 package GuessNumberHigherorLowerII.AzusaMutan;
 
-public class Solution {
+class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] memory = new int[n+1][n+1];
+        return dfs(memory, 1, n);
+    }
+
+    public int dfs (int[][] memory, int begin, int end) {
+        if (begin >= end) return 0;
+        if (memory[begin][end] != 0) return memory[begin][end];
+        int res = Integer.MAX_VALUE;
+        for (int i = begin; i <= end; i++) {
+            res = Math.min(res, i+Math.max(dfs(memory, begin, i-1),dfs(memory, i+1, end)));
+        }
+        memory[begin][end] = res;
+        return res;
+    }
 }
