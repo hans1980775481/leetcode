f7b9729570e449d10ea1b43940f7442ea05022b0
SouthLight-Lin <1171347956@qq.com>
2019-07-05 15:06:44 +0800

    SouthLight-Lin 提交链表三道题集

---
+++ src/main/java/ReverseLinkedList/SouthLight_Lin/ListNode.java
@@ -0,0 +1,16 @@
+package ReverseLinkedList.SouthLight_Lin;
+
+/**
+ * 节点
+ * @author SouthLight-Lin
+ */
+class ListNode{
+    int val;
+    ListNode next;
+    public ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
+
+
+}
