e1578ca770aa99b2cf23569a210fffc39b080410
SouthLight-Lin <1171347956@qq.com>
2019-07-06 15:46:55 +0800

    SouthLight-Lin 提交二叉树三道题集

---
+++ src/main/java/InvertBinaryTree/SouthLight_Lin/TreeNode.java
@@ -0,0 +1,14 @@
+package InvertBinaryTree.SouthLight_Lin;
+
+/**
+ * @author SouthLight-Lin on 2019/7/5
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
