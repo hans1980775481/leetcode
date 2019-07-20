2ccffbc935d8f9731f4fbe22b14db2ccadbe5a31
XMHans <1980775481@qq.com>
2019-07-20 23:50:23 +0800

    TwoSum 优化解决方法

--- src/main/java/TwoSum/XMHans/TwoSum.java
+++ src/main/java/TwoSum/XMHans/TwoSum.java
@@ -1,6 +1,7 @@
 package TwoSum.XMHans;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
@@ -42,4 +43,19 @@ public class TwoSum {
         }
         return result;
     }
+
+    /*
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ret = new int[2];
+        for (int i = 0; i< nums.length; i++) {
+            if (map.containsKey(target-nums[i])) {
+                ret[0] = map.get(target-nums[i]);
+                ret[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return ret;
+    }*/
 }
